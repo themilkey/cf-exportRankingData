c00b18a6c4e049c3670ccd015c9d3deeb2efb2e6
mi <36406142+themilkey@users.noreply.github.com>
2019-08-16 05:58:24 +0900

    fix: KDAの計算式を見直しました。

--- functions/index.js
+++ functions/index.js
@@ -2,27 +2,26 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.exportRankingData = functions.storage.object().onFinalize(async (object) => {
+exports.exportRankingData = functions.region('asia-northeast1').storage.object().onFinalize(async (object) => {
 	const fileBucket = object.bucket;
 	const filePath = object.name;
-	var database = [];
-
+	
 	if (!filePath.endsWith('MatchDto.json')) {
 		return console.log('This is not MatchDto.json');
 	}
-
+	
 	var teamName = filePath.split("/MatchDto.json")[0].split("_");
-
+	
 	const bucket = admin.storage().bucket(fileBucket);
 	bucket.file(filePath).download().then((data) => {
 		data = JSON.parse(data[0].toString());
-		console.log('gameId is ', data.gameId);
+		var database = [];
 		var bk = 0, rk = 0;
 		data.participants.forEach((p, i) => {
 			var summonerName = data.participantIdentities[i].player.summonerName
 				var player = { [summonerName] : {
 					"championId": p.championId,
-					"win": (data.teams[Math.floor(p.teamId / 200)].win === "Win") ? true : false,
+					"win": (data.teams[Math.floor(i / 5)].win === "Win") ? true : false,
 					"gameId": data.gameId,
 					"side": p.teamId,
 					"kills": p.stats.kills,
@@ -46,43 +45,39 @@ exports.exportRankingData = functions.storage.object().onFinalize(async (object)
 			var summonerName;
 			for (var k in p)  {summonerName = k;}
 			var key = p[summonerName].gameId;
-			if (p[summonerName].side == 100) {teamname = teamName[1];} else {teamname = teamName[2];}
+			if (p[summonerName].side === 100) {teamname = teamName[1];} else {teamname = teamName[2];}
 			delete p[summonerName].side;
 			delete p[summonerName].gameId;
-			var fields = {[key]: p[summonerName]};
+			var fields = {[key]: p[summonerName], "team": teamname};
 			admin.firestore().collection('data').doc(summonerName).set(fields, {merge: true});
 		});
 
-		return null;
+		return console.log('gameId is ', data.gameId);
 	})
 	.catch((err) => {
-		console.log(err);
+		return console.log(err);
 	});
 	
 	return null;
 });
 
-exports.getRanking = functions.https.onRequest((request, response) => {
+exports.getRanking = functions.region('asia-northeast1').https.onRequest((request, response) => {
 	response.set('Access-Control-Allow-Origin', '*');
-	const db = admin.firestore();
 	var tasks = [];
-	var ids = [];
+
 	var ranking = {"Kda":{}, "Dpm":{}, "Kp":{}};
-	var counter = 0;
 	Object.keys(ranking).forEach(id => {
-		tasks.push(db.collection('average').orderBy(id, 'desc').limit(Number(request.query.limit)).get());
-		ids.push(id);
+		tasks.push(admin.firestore().collection('average').orderBy(id, 'desc').limit(Number(request.query.limit)).get());
 	})
 	
 	Promise.all(tasks).then((snapshot) => {
 		snapshot.forEach((documents, index) => {
 			var top = [];
 			documents.forEach((doc, i) => {
-				top.push({"name": doc.id, "team": doc.get("Team"), "score":doc.get(ids[index])});
+				top.push({"name": doc.id, "team": doc.get("Team"), "score":doc.get(Object.keys(ranking)[index])});
 			});
-			ranking[ids[index]] = top;
+			ranking[Object.keys(ranking)[index]] = top;
 		});
-
 		return null
 	})
 	.then(() => {
@@ -95,22 +90,22 @@ exports.getRanking = functions.https.onRequest((request, response) => {
 	});
 });
 
-exports.average = functions.firestore.document('data/{summonerName}').onWrite((change, context) => {
+exports.average = functions.region('asia-northeast1').firestore.document('data/{summonerName}').onWrite((change, context) => {
 	const summonerName = context.params.summonerName;
 	const document = change.after.exists ? change.after.data() : null;
+	if (document === null) {return null;}
 
-	console.log('update player average: ', summonerName);
 	var scores = {"assists": [], "deaths": [],"dpm": [],"kills": [],"kp": []}
 	var teamName = "";
 	Object.keys(document).forEach((key, index) => {
-		if (key !== 'team') {
+		if (key === 'team') {
+			teamName = document[key];
+		} else {
 			scores.assists.push(document[key].assists);
 			scores.deaths.push(document[key].deaths);
 			scores.dpm.push(document[key].dpm);
 			scores.kills.push(document[key].kills);
 			scores.kp.push(document[key].kp);
-		} else {
-			teamName = document[key];
 		}
 	});
 	
@@ -121,12 +116,23 @@ exports.average = functions.firestore.document('data/{summonerName}').onWrite((c
 		});
 		return sum / x.length;
 	}
+	var sum = function(x) {
+		var sum = 0.0;
+		x.forEach((y) => {
+			sum += y;
+		});
+		return sum
+	}
 
-	const kda = (average(scores.kills) + average(scores.assists)) / average(scores.deaths);
+	var kda = 0;
+	if (Number(sum(scores.deaths)) === 0) {
+		kda = (sum(scores.kills) + sum(scores.assists));
+	} else {
+		kda = (sum(scores.kills) + sum(scores.assists)) / sum(scores.deaths);
+	}
 	const dpm = average(scores.dpm);
 	const kp = average(scores.kp);
 
-	admin.firestore().collection('average').doc(summonerName).set({"Kda": kda, "Dpm": dpm, "Kp": kp, "Team": teamName}, {merge: true});
-
-	return null;
+	console.log('updated player average: ', summonerName);
+	return admin.firestore().collection('average').doc(summonerName).set({"Kda": kda, "Dpm": dpm, "Kp": kp, "Team": teamName}, {merge: true});
 });
